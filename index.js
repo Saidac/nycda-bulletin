265b2de35b4a3e22e8fb487ac4c361ad871e3836
William Saidac <sdfsfsds>
2016-11-11 08:41:18 +0100

    created public dir

--- index.js
+++ index.js
@@ -8,6 +8,40 @@ const express = require('express'),
 var app = express();
     sequelize = new Sequelize('wille','wille', '', {dialect: 'postgres' });
 
+
+// var booksRouter = require('./routes/books');
+//
+// var Book = sequelize.define('book', {
+//   title: Sequelize.STRING,
+//   slug: Sequelize.STRING,
+//   imageURL: Sequelize.STRING,
+//   author: Sequelize.STRING,
+//   description: Sequelize.TEXT
+// });
+//
+
+//tell express to look for static files in public
+app.use(express.static('public'));
+
+// app.use(morgan('dev'));
+//
+// app.use(bodyParser.urlencoded({ extended: false }));
+//
+// app.use(methodOverride((req, res) => {
+//   if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+//     var method = req.body._method;
+//     delete req.body._method;
+//     return method;
+//   }})
+// );
+
+// tell express that we are using pug as view engine.
+app.set('view engine', 'pug');
+
+// Router that handl
+
+
+
 // TO DO
 //
 // views/app.pug
@@ -17,6 +51,12 @@ var app = express();
 // routes/bulletins.js
 
 
+
+app.get('/', (request, response) => {
+  response.redirect('/bulletins');
+});
+
+app.use('/books', booksRouter);
 
 
 // set listening port for server 3002
