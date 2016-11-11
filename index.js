4671def5d053bf5e7a3b199d4006f75bae8d3e6d
William Saidac <sdfsfsds>
2016-11-11 08:12:30 +0100

    added to do in index js

--- index.js
+++ index.js
@@ -7,3 +7,20 @@ const express = require('express'),
 
 var app = express();
     sequelize = new Sequelize('wille','wille', '', {dialect: 'postgres' });
+
+// TO DO
+//
+// views/app.pug
+// views/bulletins/index.pug
+// views/bulletins/new.pug
+
+// routes/bulletins.js
+
+
+
+
+// set listening port for server 3002
+// pack this thing in sequelize prototype wrapper.
+app.listen(3002, () => {
+  console.log('Web Server is running on port 3002');
+});
